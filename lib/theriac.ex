091c630877ce95c4920800861134eb2135bdb5fc
Tim de Putter <tim.de.putter83@googlemail.com>
2015-11-21 16:41:59 +0100

    remove transducer and refactoring

--- lib/theriac.ex
+++ lib/theriac.ex
@@ -1,33 +1,38 @@
 defmodule Theriac do
+
   @moduledoc """
   Theriac is a implementation of clojure style transducers in elixir
   """
 
   def transduce enum, transducer do
-    reducer = transducer.(fn {r, input} -> r ++ [input] end)
-    Enum.reduce(enum,[], fn(x,acc) -> reducer.({acc,x}) end)
+    reducer = transducer.(fn {result, input} -> result ++ [input] end)
+    Enum.reduce(enum,[], fn(input,result) -> reducer.({result,input}) end)
   end
 
   def map f do
-    fn rf ->
-      fn
-        {} -> rf.()
-        {result} -> rf.({result})
-        {result, input} -> rf.({result, f.(input)})
-      end
+    stateless_transducer fn 
+      rf, result, input -> rf.({result, f.(input)}) 
+    end
+  end
+
+  def remove f do
+    stateless_transducer fn 
+      rf, result, input -> unless f.(input), do: rf.({result,input}), else: result 
     end
   end
 
   def filter f do
+    stateless_transducer fn 
+      rf, result, input -> if f.(input), do: rf.({result,input}), else: result
+    end
+  end
+
+  defp stateless_transducer reduction do
     fn rf ->
       fn
         {} -> rf.()
         {result} -> rf.({result})
-        {result, input} -> 
-          if f.(input) do
-            result = rf.({result,input})
-          end
-          result
+        {result, input} -> reduction.(rf, result, input)
       end
     end
   end
--- test/theriac_test.exs
+++ test/theriac_test.exs
@@ -2,14 +2,19 @@ defmodule TheriacTest do
   use ExUnit.Case
   import Theriac
 
-  test "mapping transducer" do
+  test "map" do
     result = transduce([1,2,3],map(fn inp -> inp + 1 end))
     assert result == [2,3,4]
   end
 
-  test "filtering transducer" do
+  test "filter" do
     result = transduce([1,2,3],filter(fn inp -> inp > 2 end))
     assert result == [3]
+  end
+
+  test "remove" do
+    result = transduce([1,2,3],remove(fn inp -> inp > 2 end))
+    assert result == [1,2]
   end
 
 end
