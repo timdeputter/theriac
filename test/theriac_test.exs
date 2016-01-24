79bfc8a041014d2e44346e4754c4702a0bd700d9
Tim de Putter <tim.de.putter83@googlemail.com>
2016-01-24 13:26:04 +0100

    some more tests for take

--- test/theriac_test.exs
+++ test/theriac_test.exs
@@ -3,6 +3,14 @@ defmodule TheriacTest do
   doctest Theriac
   import Theriac
 
+  test "take no element" do
+    assert transduce([1,2,3], take(0)) == []
+  end
+
+  test "taking negative elements is equal to taking no element" do
+    assert transduce([1,2,3], take(-7)) == []
+  end
+
   test "combining multiple stateless transducers" do
     transducer = comb [filter(fn i -> i < 10 end), map(fn i -> i * 2 end)]
     result = transduce([1,2,7,9,11,22,3,10], transducer)
